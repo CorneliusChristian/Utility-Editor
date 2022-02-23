938d49086734b6bf873be5806ba1583c11787fc7
CorneliusChristian <78033120+CorneliusChristian@users.noreply.github.com>
2022-02-23 19:08:21 +0700

    Add files via upload

---
+++ Utility Editor/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ipme = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pros = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(17, 104);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(199, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Task Manager Enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Utility Editor - 2C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Settings : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 72);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save Setting";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(17, 152);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(207, 24);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Show Seconds on Clock";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(17, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(199, 31);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete Temporary Files";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(199, 31);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Delete Prefetch";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ipme
+            // 
+            this.ipme.AutoSize = true;
+            this.ipme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipme.Location = new System.Drawing.Point(12, 512);
+            this.ipme.Name = "ipme";
+            this.ipme.Size = new System.Drawing.Size(92, 25);
+            this.ipme.TabIndex = 8;
+            this.ipme.Text = "Your IP : ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Refresh Explorer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(5, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 407);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Utilities";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.pros);
+            this.groupBox2.Location = new System.Drawing.Point(243, 41);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(218, 407);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Process";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // pros
+            // 
+            this.pros.Location = new System.Drawing.Point(28, 61);
+            this.pros.Name = "pros";
+            this.pros.Size = new System.Drawing.Size(159, 26);
+            this.pros.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Process name (.exe) :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(28, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 33);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Kill Process";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(28, 239);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(159, 33);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Check Process";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(28, 336);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(159, 33);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "Run Process";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 549);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ipme);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Utility Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label ipme;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pros;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Utility Editor/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey key =   Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            if (checkBox1.Checked)
+            {
+                if(key.GetValue("DisableTaskMgr") != null)
+                {
+                    key.DeleteValue("DisableTaskMgr");
+                    MessageBox.Show("Settings are saved.");
+                }
+                else
+                {
+                    MessageBox.Show("Task Manager is already enabled");
+                }
+            }
+            else
+            {
+                key.SetValue("DisableTaskMgr", "1");
+                MessageBox.Show("Settings are saved.");
+            }
+            RegistryKey key2 = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced");
+            if (checkBox2.Checked)
+            {
+                key2.SetValue("ShowSecondsInSystemClock", 1, RegistryValueKind.DWord);
+            }
+            else
+            {
+                key2.SetValue("ShowSecondsInSystemClock", 0, RegistryValueKind.DWord);
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        { 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(@"C:\Windows\Temp");
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            
+            catch (IOException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+            }
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(@"C:\Windows\Temp");
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+
+                
+            }
+
+            catch (IOException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+            }
+
+            try
+            {
+                System.IO.DirectoryInfo dis = new DirectoryInfo(@"C:\Users\" + Environment.UserName + @"\AppData\Local\Temp");
+                foreach (FileInfo file in dis.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in dis.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            catch (IOException nn)
+            {
+                Console.WriteLine(nn.Message);
+            }
+            MessageBox.Show("Task completed.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.IO.DirectoryInfo di2 = new DirectoryInfo(@"C:\Windows\Prefetch");
+                foreach (FileInfo file in di2.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di2.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            catch(IOException nn)
+            {
+                Console.WriteLine(nn.Message);
+            }
+            MessageBox.Show("Task completed.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string IPADDRESS = new WebClient().DownloadString("https://ifconfig.me/ip");
+                ipme.Text = "Your IP : " + IPADDRESS;
+            }
+            catch(System.Net.WebException)
+            {
+                MessageBox.Show("Warning!, IP Address will not show because of the internet connection.");
+            }
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Process.GetProcessesByName("explorer").Length > 0)
+            {
+                Process.Start("taskkill", "/F /IM explorer.exe");
+                Thread.Sleep(2000);
+                Process.Start("explorer");
+            }
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.Start("taskkill", "/f /im " + pros.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Process.Start(pros.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string a = pros.Text;
+            if (a.Contains(".exe"))
+            {
+                a = a.Substring(0, a.Length - 4);
+            }
+            if(Process.GetProcessesByName(a).Length > 0)
+            {
+                MessageBox.Show("The process is running.");
+            }
+            else
+            {
+                MessageBox.Show("The process doesn't exist");
+            }
+        }
+    }
+}
